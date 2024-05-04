72120e8c7929511bd5f36eb8a6999cd10fbadf1a
JustYoussef <167704295+HellD4nGer@users.noreply.github.com>
2024-05-04 19:20:49 +0300

    working on search pt1

--- My-App-Project/app/(tabs)/Home.tsx
+++ My-App-Project/app/(tabs)/Home.tsx
@@ -1,90 +1,124 @@
-import { View, Text,StyleSheet, TouchableOpacity ,Image,Pressable, TextInput, ScrollView} from 'react-native'
-import React from 'react'
-import { Link, Stack } from 'expo-router'
-import { Feather } from '@expo/vector-icons';
-import { Entypo } from '@expo/vector-icons';
-import color from '@/constans/color';
-import  { useRef, useState } from "react";
-
-import{useHeaderHeight} from'@react-navigation/elements'
-import { AntDesign } from '@expo/vector-icons';
-import CatogreyBtn from '@/component/CatogreyBtn';
-import Listings from './Listings';
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  Image,
+  Pressable,
+  TextInput,
+  ScrollView,
+} from "react-native";
+import React from "react";
+import { Link, Stack } from "expo-router";
+import { Feather } from "@expo/vector-icons";
+import { Entypo } from "@expo/vector-icons";
+import color from "@/constans/color";
+import { useRef, useState } from "react";
+import { useHeaderHeight } from "@react-navigation/elements";
+import { AntDesign } from "@expo/vector-icons";
+import CatogreyBtn from "@/component/CatogreyBtn";
+import Listings from "./Listings";
 import listingData from "@/data/destinations.json";
-import GroupListings from '@/component/GroupListings';
-import groupData from "@/data/groups.json"
+import GroupListings from "@/component/GroupListings";
+import groupData from "@/data/groups.json";
 export default function Home() {
-  const headerHight=useHeaderHeight();
-   const [category, setCategory] = useState("All");
+  const headerHight = useHeaderHeight();
+  const [category, setCategory] = useState("All");
 
   const onCatChanged = (category: string) => {
     console.log("Categpry: ", category);
     setCategory(category);
   };
   return (
-  <>  <Stack.Screen options={{
-      headerTransparent:true,
-      headerTitle:"",
-      headerLeft:()=>(
-        <TouchableOpacity onPress={()=>{}} style={{marginLeft:1400}}>
-          <Feather name="menu" size={24} color="black" style={{width:40,height:40,paddingLeft:20}} />
-        
-        </TouchableOpacity>
-      ),
-headerRight:()=>(
-        
-        <Link href={`/login`} asChild><TouchableOpacity onPress={()=>{{}}} style={{marginRight:1400,backgroundColor:color.white,shadowColor:'#171717',width:35,height:35,paddingTop:5,borderRadius:5,shadowOffset:{width:2,height:4},shadowOpacity:0.2,shadowRadius:3}}>
-              
-             <Entypo name="login" size={26} color="black"  />
-            </TouchableOpacity></Link>
-),
+    <>
+      {" "}
+      <Stack.Screen
+        options={{
+          headerTransparent: true,
+          headerTitle: "",
+          headerLeft: () => (
+            <TouchableOpacity onPress={() => {}} style={{ marginLeft: 1400 }}>
+              <Feather
+                name="menu"
+                size={24}
+                color="black"
+                style={{ width: 40, height: 40, paddingLeft: 20 }}
+              />
+            </TouchableOpacity>
+          ),
+          headerRight: () => (
+            <Link href={`/login`} asChild>
+              <TouchableOpacity
+                onPress={() => {
+                  {
+                  }
+                }}
+                style={{
+                  marginRight: 1400,
+                  backgroundColor: color.white,
+                  shadowColor: "#171717",
+                  width: 35,
+                  height: 35,
+                  paddingTop: 5,
+                  borderRadius: 5,
+                  shadowOffset: { width: 2, height: 4 },
+                  shadowOpacity: 0.2,
+                  shadowRadius: 3,
+                }}
+              >
+                <Entypo name="login" size={26} color="black" />
+              </TouchableOpacity>
+            </Link>
+          ),
+        }}
+      />
+      <View style={[styles.container, { paddingTop: headerHight }]}>
+        <Text style={styles.headingText}>Explore The Beatifaul App!</Text>
 
+        <View style={styles.searchSectionWrapper}>
+          <View style={styles.searchBar}>
+            <AntDesign
+              name="search1"
+              style={{ marginRight: 18, paddingLeft: 15, width: 150 }}
+              size={24}
+              color={color.black}
+            />
+            <TextInput placeholder="Search...."></TextInput>
+          </View>
+        </View>
 
-    }}
-    />
-    <View style={[styles.container,{paddingTop:headerHight}]}>
-      <Text style={styles.headingText}>Explore The  Beatifaul App!</Text>
-    <View style={styles.searchSectionWrapper}>
-      <View style={styles.searchBar}>
-        <AntDesign name="search1" style={{marginRight:18,paddingLeft:15,width:150}} size={24} color={color.black} />
-<TextInput placeholder='Search....'></TextInput>
+        <CatogreyBtn onCagtegoryChanged={onCatChanged} />
+
+        <ScrollView>
+          <Listings listings={listingData} category={category} />
+          <Listings listings={listingData} category={category} />
+
+          <GroupListings listings={groupData} />
+        </ScrollView>
       </View>
-
-    </View>
-<CatogreyBtn onCagtegoryChanged={onCatChanged}/>  
-
-<ScrollView>
-<Listings listings={listingData} category={category} />
-<Listings listings={listingData} category={category} />
-
-
-<GroupListings listings={groupData}/>
-</ScrollView>
-  </View>
-  
     </>
-  )
+  );
 }
-const styles=StyleSheet.create({
-    container:{
-        flex:1,
-       paddingHorizontal:20,
-       backgroundColor:color.bgColor,
-    },
-    headingText:{
-      fontSize:28,
-      fontWeight:'800',
-      color:color.black,
-      marginTop:10,
-    },
-    searchSectionWrapper:{
-flexDirection:'row',
-marginVertical:20,
-    },
-    searchBar:{
-      flexDirection:'row',
-      backgroundColor:color.white,
-      padding:16,
-      borderRadius:10
-    }
-})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingHorizontal: 20,
+    backgroundColor: color.bgColor,
+  },
+  headingText: {
+    fontSize: 28,
+    fontWeight: "800",
+    color: color.black,
+    marginTop: 10,
+  },
+  searchSectionWrapper: {
+    flexDirection: "row",
+    marginVertical: 20,
+  },
+  searchBar: {
+    flexDirection: "row",
+    backgroundColor: color.white,
+    padding: 16,
+    borderRadius: 10,
+  },
+});
--- My-App-Project/app/(tabs)/_layout.tsx
+++ My-App-Project/app/(tabs)/_layout.tsx
@@ -1,44 +1,48 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-import { AntDesign, Ionicons } from '@expo/vector-icons';
-import { Fontisto } from '@expo/vector-icons';
-import { Tabs } from 'expo-router'
-import color from '@/constans/color';
+import { View, Text } from "react-native";
+import React from "react";
+import { AntDesign, Ionicons } from "@expo/vector-icons";
+import { Fontisto } from "@expo/vector-icons";
+import { Tabs } from "expo-router";
+import color from "../../constans/color";
 
 export default function _layout() {
   return (
-    <Tabs screenOptions={{
-        tabBarStyle:{
-            backgroundColor:color.bgColor,
-            borderTopWidth:0,
-            padding:0
+    <Tabs
+      screenOptions={{
+        tabBarStyle: {
+          backgroundColor: color.bgColor,
+          borderTopWidth: 0,
+          padding: 0,
         },
-        tabBarShowLabel:false,
-        tabBarActiveTintColor:color.black,
-        tabBarInactiveTintColor:'#999'
-    }}>
-<Tabs.Screen name='Home' options={{
-    tabBarIcon:({color})=>(
-        <Ionicons name="home" size={24} color="#ff7f36" />
-
-    )
-}}/>
-<Tabs.Screen name='cart'options={{
-    tabBarIcon:({color})=>(
-        <Ionicons name="cart" size={24} color="black" />
-
-
-    )
-}}/>
-<Tabs.Screen name='profile'options={{
-    tabBarIcon:({color})=>(
-        <Ionicons name="person" size={24} color="black" />
-
-
-    )
-}}/>
-
-    
+        tabBarShowLabel: false,
+        tabBarActiveTintColor: color.black,
+        tabBarInactiveTintColor: "#999",
+      }}
+    >
+      <Tabs.Screen
+        name="Home"
+        options={{
+          tabBarIcon: ({ color }) => (
+            <Ionicons name="home" size={24} color="#ff7f36" />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="cart"
+        options={{
+          tabBarIcon: ({ color }) => (
+            <Ionicons name="cart" size={24} color="black" />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="profile"
+        options={{
+          tabBarIcon: ({ color }) => (
+            <Ionicons name="person" size={24} color="black" />
+          ),
+        }}
+      />
     </Tabs>
-  )
+  );
 }
